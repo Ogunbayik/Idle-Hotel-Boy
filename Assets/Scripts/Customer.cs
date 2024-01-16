4795725442d8b6c8d5f2ea43742faa46aa544a51
Ogunbayik <ogun.bayikk@gmail.com>
2024-01-16 22:43:54 +0300

    Customer fix

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -5,21 +5,87 @@ using UnityEngine.AI;
 
 public class Customer : MonoBehaviour
 {
+    private enum States
+    {
+        Wait,
+        Move
+    }
+
+    private States currentState;
+
     private NavMeshAgent agent;
 
-    [SerializeField] private Transform desiredPoint;
+    private Transform desiredPoint;
+    [SerializeField] private Transform[] movementPoints;
+
+    private int moveIndex = 0;
+
+    private float distanceToPoint;
+    private float startWaitTime = 3f;
+    private float waitTimer;
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        currentState = States.Move;
     }
 
     void Start()
     {
-        
+        waitTimer = startWaitTime;
+        distanceToPoint = GetDistanceMovePoint(movementPoints[moveIndex]);
     }
 
     void Update()
     {
-        agent.SetDestination(desiredPoint.position);
+        switch (currentState)
+        {
+            case States.Wait:
+                Waiting();
+                break;
+            case States.Move:
+                Moving(movementPoints[moveIndex]);
+                break;
+        }
+
+        if (distanceToPoint > 0.2f)
+        {
+            currentState = States.Move;
+        }
+        else
+        {
+            currentState = States.Wait;
+        }
     }
+
+    private void Waiting()
+    {
+        waitTimer -= Time.deltaTime;
+        
+        if(waitTimer <= 0)
+        {
+            GoNextDirection();
+            waitTimer = startWaitTime;
+        }
+    }
+
+    private void Moving(Transform transform)
+    {
+        distanceToPoint = GetDistanceMovePoint(movementPoints[moveIndex]);
+        agent.SetDestination(transform.position);
+    }
+    private void GoNextDirection()
+    {
+        moveIndex++;
+        desiredPoint = movementPoints[moveIndex];
+        currentState = States.Move;
+        GetDistanceMovePoint(desiredPoint);
+    }
+
+    private float GetDistanceMovePoint(Transform point)
+    {
+        distanceToPoint = 100f;
+        distanceToPoint = Vector3.Distance(transform.position, point.transform.position);
+        return distanceToPoint;
+    }
+
 }
